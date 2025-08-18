318c0cfcb4188e9a9893a229bff1b07b948a30d9
fertas2802 <fertas2009@gmail.com>
2025-08-18 12:29:16 -0300

    cambios al mail para usar PORT dinamico

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,25 @@
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        // Render define el puerto en la variable de entorno PORT
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+        Server server = new Server(port);
 
         ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         handler.setContextPath("/");
 
+        // Servlets
+        handler.addServlet(new ServletHolder(new HelloServlet()), "/hola");
+        handler.addServlet(new ServletHolder(new GuardarServlet()), "/guardar");
 
-    // Asociar el servlet con la ruta "/hola"
-    handler.addServlet(new ServletHolder(new HelloServlet()), "/hola");
-
-    // Asociar el servlet con la ruta "/guardar"
-    handler.addServlet(new ServletHolder(new GuardarServlet()), "/guardar");
-
-        // Configurar servlet para archivos estáticos
+        // Archivos estáticos
         ServletHolder defaultServlet = new ServletHolder("default", DefaultServlet.class);
         defaultServlet.setInitParameter("resourceBase", "src/main/webapp");
         defaultServlet.setInitParameter("dirAllowed", "true");
         handler.addServlet(defaultServlet, "/");
 
         server.setHandler(handler);
-        
-        System.out.println("Servidor iniciado en http://0.0.0.0:8080");
+
+        System.out.println("Servidor iniciado en http://0.0.0.0:" + port);
         server.start();
         server.join();
     }
