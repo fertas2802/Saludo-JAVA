f119f067f8cce4d55e4224ab19757d1d032de3fb
fertas2802 <fertas2009@gmail.com>
2025-08-18 00:40:25 -0300

    Actualizar imports a jakarta.servlet

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -1,5 +1,5 @@
-import javax.servlet.*;
-import javax.servlet.http.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 import java.io.*;
 
 public class HelloServlet extends HttpServlet {
